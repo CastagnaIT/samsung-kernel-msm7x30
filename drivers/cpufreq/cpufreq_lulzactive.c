7e15aeb10a0e367aed6f5b02dfdca8a0820df082
CastagnaIT <whiteflash@email.it>
2013-01-10 18:57:01 +0100

    cpufreq: added all governor with some tweaks/corrections

--- drivers/cpufreq/cpufreq_lulzactive.c
+++ drivers/cpufreq/cpufreq_lulzactive.c
@@ -238,7 +238,7 @@ static int cpufreq_governor_lulzactive(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_lulzactive = {
-	.name = "lulzactive",
+	.name = "Lulzactive",
 	.governor = cpufreq_governor_lulzactive,
 	.max_transition_latency = 9000000,
 	.owner = THIS_MODULE,
