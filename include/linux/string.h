b747c53f83249b668378985424c5a36e0e31fbb9
CastagnaIT <whiteflash@email.it>
2013-05-25 00:31:57 +0200

    string: added custom substr string function

--- include/linux/string.h
+++ include/linux/string.h
@@ -114,6 +114,9 @@ extern int memcmp(const void *,const void *,__kernel_size_t);
 #ifndef __HAVE_ARCH_MEMCHR
 extern void * memchr(const void *,int,__kernel_size_t);
 #endif
+#ifndef __HAVE_ARCH_SUBSTR
+extern char * substr(const char *s, int pos, int len);
+#endif
 void *memchr_inv(const void *s, int c, size_t n);
 
 extern char *kstrdup(const char *s, gfp_t gfp);
